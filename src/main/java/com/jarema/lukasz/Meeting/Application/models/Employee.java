b0389dca1e8212e80258960e2f5989508be3c02b
Łukasz Jarema <lukasz.jarema@live.com>
2023-03-20 14:40:07 +0100

    Utworzenie modelu Pracownika jako encja bazy danych.

---
+++ src/main/java/com/jarema/lukasz/Meeting/Application/models/Employee.java
@@ -0,0 +1,23 @@
+package com.jarema.lukasz.Meeting.Application.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String surname;
+    private String emailAddress;
+    private int telephoneNumber;
+}
