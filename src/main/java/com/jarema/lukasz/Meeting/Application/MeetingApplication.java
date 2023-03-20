aa0974d9c4bdf6c29f9414927c49f1cc02056de9
Łukasz Jarema <lukasz.jarema@live.com>
2023-03-20 13:54:44 +0100

    Initial commit

---
+++ src/main/java/com/jarema/lukasz/Meeting/Application/MeetingApplication.java
@@ -0,0 +1,13 @@
+package com.jarema.lukasz.Meeting.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarema/lukasz/Meeting/Application/MeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarema.lukasz.Meeting.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
