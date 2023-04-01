2661b2c79b5d38253a00fcdb257bdbd25e4425c6
Łukasz Jarema <lukasz.jarema@live.com>
2023-04-01 14:34:16 +0200

    Modyfikacja widoku meetings-create.html oraz utworzenie endpointa GetMapping /meeting

--- src/main/java/com/jarema/lukasz/Meeting/Application/controllers/MeetingController.java
+++ src/main/java/com/jarema/lukasz/Meeting/Application/controllers/MeetingController.java
@@ -3,6 +3,7 @@ package com.jarema.lukasz.Meeting.Application.controllers;
 import com.jarema.lukasz.Meeting.Application.dtos.EmployeeDto;
 import com.jarema.lukasz.Meeting.Application.models.Employee;
 import com.jarema.lukasz.Meeting.Application.models.Meeting;
+import com.jarema.lukasz.Meeting.Application.repositories.EmployeeRepository;
 import com.jarema.lukasz.Meeting.Application.services.EmployeeService;
 import com.jarema.lukasz.Meeting.Application.services.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +17,9 @@ import java.util.List;
 @Controller
 public class MeetingController {
     private MeetingService meetingService;
-    private EmployeeService employeeService;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Autowired
     public MeetingController(MeetingService meetingService) {
@@ -25,12 +28,10 @@ public class MeetingController {
 
     @GetMapping("/meeting")
     public String createMeeting(Model model) {
-        List<EmployeeDto> employeeList = employeeService.findAllEmployees();
-        model.addAttribute("meeting", new Meeting());
-        model.addAttribute("employeeList", employeeList);
-
-        for (int i = 0; i < employeeList.size(); i++)
-            System.out.printf(String.valueOf(employeeList.get(i)));
+        List<Employee> employeesList = employeeRepository.findAll();
+        Meeting meeting = new Meeting();
+        model.addAttribute("meeting", meeting);
+        model.addAttribute("employeeList", employeesList);
 
         return "meetings-create";
     }
