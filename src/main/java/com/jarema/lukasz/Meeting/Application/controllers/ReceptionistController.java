5ada853c69dfa8373c39bf49d73e4e6cdcfe8567
Łukasz Jarema <lukasz.jarema@live.com>
2023-04-23 12:26:50 +0200

    Modyfikacja w plikach widoku

--- src/main/java/com/jarema/lukasz/Meeting/Application/controllers/ReceptionistController.java
+++ src/main/java/com/jarema/lukasz/Meeting/Application/controllers/ReceptionistController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ReceptionistController {
 
-    @GetMapping("/receptionist/login")
+    @GetMapping("/receptionists/login")
     public String loginAsAReceptionistPage() {
         return "receptionists-login";
     }
